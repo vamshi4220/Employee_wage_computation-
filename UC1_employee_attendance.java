dc3e7c8412933f4397a2f976463d9296cd9509e7
vamshi4220 <ud75281@gmail.com>
2020-10-29 13:34:11 +0530

    employee attendance is checked

---
+++ UC1_employee_attendance.java
@@ -0,0 +1,11 @@
+public class uc1_employee_attendance {
+
+	public static void main(String[] args) {
+		 if(Math.random()<0.5) {
+			 System.out.println("Employee is absent");
+		 }
+		 else System.out.println("Employee is present");
+     
+	}
+
+}
