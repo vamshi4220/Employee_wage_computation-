f11864d53c5bd953921dad5287a73bf6c9dd1f46
vamshi4220 <ud75281@gmail.com>
2020-10-29 15:18:45 +0530

    employee wage is calculated

--- UC1_employee_attendance.java
+++ UC1_employee_attendance.java
@@ -1,3 +1,4 @@
+// UC1 ATTENDANCE
 public class uc1_employee_attendance {
 
 	public static void main(String[] args) {
@@ -6,6 +7,18 @@ public class uc1_employee_attendance {
 		 }
 		 else System.out.println("Employee is present");
      
+	}
+
+}
+
+//UC2 EMPLOYEE WAGE
+
+public class UC2_employee_wage {
+
+	public static void main(String[] args) {
+		int Wage_per_hr=20;
+		int num_hr=8;
+		System.out.println("daily wage of the employee is:"+Wage_per_hr*num_hr );
 	}
 
 }
--- UC2_employee_wage.java
+++
@@ -1,9 +0,0 @@
-public class UC2_employee_wage {
-
-	public static void main(String[] args) {
-		int Wage_per_hr=20;
-		int num_hr=8;
-		System.out.println("daily wage of the employee is:"+Wage_per_hr*num_hr );
-	}
-
-}
