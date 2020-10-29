b36ac793a2d71115cd0f8a3e1bf0e08705c52049
vamshi4220 <ud75281@gmail.com>
2020-10-29 17:19:56 +0530

    added part time wage

--- UC1_employee_attendance.java
+++ UC1_employee_attendance.java
@@ -9,13 +9,18 @@ public class uc1_employee_attendance {
 	}
 
 //UC2 EMPLOYEE WAGE
+//UC3 ADDED PART TIME EMPLOYEE
    
        void employee_wage()  {
 		
 		int Wage_per_hr=20;
-		int num_hr=8;
-		System.out.println("daily wage of the employee is:"+Wage_per_hr*num_hr );
+		int num_hr_FULL_TIME=8;
+                int num_hr_PART_TIME=4;
+		System.out.println("daily wage of the employee is:"+Wage_per_hr*num_hr_FULL_TIME );
+                System.out.println("daily wage of the employee is:"+Wage_per_hr*num_hr_PART_TIME );
 	}
+
+
 
 	public static void main(String[] args) {
                 new uc1_employee_attendance().Attendance();
