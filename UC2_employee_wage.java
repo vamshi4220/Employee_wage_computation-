c60addf0039c465fb2d549dc41c221752d8e025e
vamshi4220 <ud75281@gmail.com>
2020-10-29 13:51:34 +0530

    employee wage is calculated

---
+++ UC2_employee_wage.java
@@ -0,0 +1,9 @@
+public class UC2_employee_wage {
+
+	public static void main(String[] args) {
+		int Wage_per_hr=20;
+		int num_hr=8;
+		System.out.println("daily wage of the employee is:"+Wage_per_hr*num_hr );
+	}
+
+}
